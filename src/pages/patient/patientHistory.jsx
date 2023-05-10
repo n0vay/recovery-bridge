32707bf47b0ec4d8bb1ca3a474f6d597aee43c69
n0vay <koshantanu@gmail.com>
2023-05-10 01:14:48 -0400

    updated pages

--- src/pages/patient/patientHistory.jsx
+++ src/pages/patient/patientHistory.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Sidenav from "../../components/sidenav";
 import Navbar from "../../components/navbar";
 import { Box } from "@mui/system";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 function PatientHistory() {
   return (
@@ -12,6 +18,35 @@ function PatientHistory() {
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <div>Patient History</div>
+          <Timeline position="alternate">
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>Eat</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>Code</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>Sleep</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Repeat</TimelineContent>
+            </TimelineItem>
+          </Timeline>
         </Box>
       </Box>
     </>
--- src/pages/patient/patientInformation.jsx
+++ src/pages/patient/patientInformation.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Sidenav from "../../components/sidenav";
 import Navbar from "../../components/navbar";
 import { Box } from "@mui/system";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 function PatientInformation() {
   return (
@@ -11,7 +14,15 @@ function PatientInformation() {
       <Box sx={{ display: "flex" }}>
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <div>Patient Information</div>
+        <Typography variant="h4" gutterBottom>
+            Patient Information
+          </Typography>
+          <Box height={10} />
+          <Avatar
+            alt='Salman'
+            srcSet="http://images.catchnews.com/upload/2015/10/09/salman-khan-in-tere-naam_127994.jpg"
+            sx={{ width: 150, height: 150 }}
+          />
         </Box>
       </Box>
     </>
